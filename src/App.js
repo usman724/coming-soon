e1438665474b7cb742e46fcc9f4e3c7d93aab704
usman724 <usmanpervaiz724@gmail.com>
2024-04-27 00:00:07 +0000

    add fix

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 import React from 'react';
 import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
-import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
-import "./App.css";
+import './App.css';
+import Logo from './logo_details.png';
+import Modal from './modal.png';
 
 class App extends React.Component {
   render = () => {
     return (
-      <div className="card">
-        <div className="header">
-          <div className="logo">
-            <a href=".">LOGO</a>
+      <div className='app-container'>
+        <div className='card'>
+          <div className='header'>
+            <div className='logo'>
+              <img src={Logo} alt='Logo' />
+            </div>
+            <div className='social'>
+              <a href='https://web.facebook.com/mzmartz' title='Facebook' target='_blank' rel='noopener noreferrer'>
+                <IconFacebook className='icon' />
+              </a>
+            </div>
           </div>
-          <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
-              <IconFacebook className="icon" />
-            </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
-            </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
-            </a>
-          </div>
-        </div>
-        <div className="content">
-          <div className="title-holder">
+          <div className='content'>
+            <img src={Modal} alt='Modal' />
             <h1>Get ready for the change.</h1>
             <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <a href='mailto:support@mzmarts.com'>
+              <div className='cta'>Send us an email</div>
+            </a>
           </div>
-          <a href="mailto:user@example.com">
-            <div className="cta">Send us an email</div>
-          </a>
-        </div>
-        <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
         </div>
       </div>
     );
